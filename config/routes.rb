5264c6776cae23daa30731ed34c6daade836b42e
jamesncox <jamesncox@yahoo.com>
2019-12-05 12:14:54 -0600

    Refactor resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
   namespace :api do
     namespace :v1 do
-
       resources :lists
       resources :stores
-
     end
   end
 end
