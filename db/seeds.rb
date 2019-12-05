3717ff4512949e94c23760bc186610ca3d0c0af1
jamesncox <jamesncox@yahoo.com>
2019-12-05 11:40:26 -0600

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 1.times do 
+#     Store.create({name: "Kroger"})
+#     puts "Created"
+# end
+
+# 3.times do 
+#     List.create({item: "Monday", price: 3.99, quantity: "100", store_id: 1})
+# end 
+
+Store.create!([{ name: 'Kroger' }, { name: 'Target' }, { name: 'Lucky' }])
+
+List.create(item: 'Milk', price: 3.99, quantity: 1, store_id: 1)
+List.create(item: 'Quilt', price: 70.00, quantity: 2, store_id: 2)
+List.create(item: 'Bomber Jacket', price: 150.69, quantity: 1, store_id: 3,)
+List.create(item: 'Skinny Jeans', price: 85.99, quantity: 1, store_id: 3,)
+List.create(item: 'Top Gun T-shirt', price: 35.99, quantity: 1, store_id: 3,)
